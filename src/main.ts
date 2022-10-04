440cc5c972b609b008625304de19a73ff44c101a
jdm1219 <jdm1219@fixelsoft.com>
2022-10-04 11:20:54 +0900

    chore: helmet 추가

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('🎂deco my cake🍰')
